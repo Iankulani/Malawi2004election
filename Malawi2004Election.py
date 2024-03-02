29922e2dee2a1715a3b41668b9c7e730749b6e25
Ian Carter Kulani <161088906+Iankulani@users.noreply.github.com>
2024-03-02 22:45:40 +0200

    Update Malawi2004Election.py
    This software prompts the user to enter total number of published centers, total number of registered votes, total number of null_&_void votes, total number of valid votes and total valid votes for each candidate. Afterward, it determines the candidate with the majority of votes and displays the results on the screen.
    NOTE: For a candidate to be a majority winner the candidate total valid votes should be greater than majority votes.

--- Malawi2004Election.py
+++ Malawi2004Election.py
@@ -5,26 +5,28 @@ Created on Sat Feb 17 02:31:52 2024
 
 @author: IAN CARTER KULANI
 """
+#This software prompts the user to enter total number of published centers,total number of registered votes, total number of null and void votes, total number of valid votes and total valid votes for each candidate. Afterward,it determines the candidate with the majority of votes and displays the results on the screen.
+#NOTE:For a candidate to be a majority winner the candidate total valid votes should be greater than majority votes.
+print("================================MALAWI ELECTROL COMMISSION================================\n")
 
-print("==============================MALAWI ELECTROL COMMISSION==============================\n") 
 Totalpublishedcenters=int(input("Enter Total published centers:"))
 #Get the total number of registered votes
-TotalRegisteredvotes=int(input("Enter Total Registered Votes:"))
+TotalRegisteredvotes=int(input("Enter Total Registered Voters/turnout:"))
 #Get Total number of votes cast
-Totalvotescast=int(input("Enter Total Votes Cast:"))
+Totalvotescast=int(input("Enter Total Votes Cast/Total Votes:"))
 #Get total number of Null_&_Void votes
-Nullandvoid=int(input("Enter Total Null and Void Votes:"))
+Nullandvoid=int(input("Enter Total Null_&_Void Votes:"))
 #Get Total Valid Votes for All candidates
 Totalvalidvotes=int(input("Enter Total Valid Votes:"))
-#
+#Get Total Valid Votes For Bingu Wa Mutharika
 Totalvalidvotesfor_Bingumutharika=int(input("Enter Total Valid Votes For Bingu Wa Mutharika:"))
-#
+#Get Total Valid Votes For John Tembo
 Totalvalidvotesfor_Johntembo=int(input("Enter Total Valid Votes For John Tembo:"))
-#
+#Get Total Valid Votes For Gwanda Chakuamba
 Totalvalidvotesfor_Gwandachakuamba=int(input("Enter Total Valid Votes For Gwanda Chakuamba:"))
-#
+#Get Total Valid Votes For Brown Mpinganjira
 Totalvalidvotesfor_Brownmpinganjira=int(input("Enter Total Valid Votes For Brown Mpinganjira:"))
-#
+#Get Total Valid Votes For Jusice Malewezi
 Totalvalidvotesfor_Justicemalewezi=int(input("Enter Total Valid Votes For Jusice Malewezi:"))
 #
 percent=100
@@ -49,15 +51,36 @@ elif Totalvalidvotesfor_Justicemalewezi>Totalvalidvotes/2+1:
 
 else:
       print("No majority winner was found RUNOF may be required Thank you.\n\n")
+      
+print("____________________________________ELECTION STATISTICS___________________________________\n")  
 
 #Calculating Majority
-majority_percent=Totalvalidvotesfor_Bingumutharika*percent/Totalvalidvotes;
-print("Total Valid Votes For Bingu Wa Mutharika in percentage=",majority_percent)
-majority=Totalvalidvotesfor_Johntembo*percent/Totalvalidvotes;
-print("Total Valid Votes For John Tembo in percent=",majority_percent)
-majority=Totalvalidvotesfor_Gwandachakuamba*percent/Totalvalidvotes;
-print("Total Valid Votes For Gwanda Chakuamba=",majority_percent)
-majority=Totalvalidvotesfor_Brownmpinganjira*percent/Totalvalidvotes;
-print("Total Valid Votes For Brown Mpinganjira=",majority_percent)
-majority=Totalvalidvotesfor_Justicemalewezi*percent/Totalvalidvotes;
-print("Total Valid Votes For Jusice Malewezi=",majority_percent)
+#Calculating percentage for total votes cast 
+Percentage=round(Totalvalidvotes*percent/Totalvalidvotes, 2);
+print("Total Votes Cast in percentage=",Percentage)
+#Calculating percentage for total valid votes for all canidates
+Percentage=round(Totalvalidvotes*percent/Totalvotescast, 2);
+print("Total Valid Votes for all candidtes in percentage=",Percentage)
+#Calculating percentage for null_&_void votes
+Percentage=round(Nullandvoid*percent/Totalvalidvotes, 2);
+print("Total Null_&_Void in percentage=",Percentage)
+#Calculating percentage for Total Registered voters/turnout
+Percentage=round(Totalvotescast*percent/TotalRegisteredvotes, 2);
+print("Total Registered voters/turnout in percentage=",Percentage)
+#Calculating percentage for Bingu Wa Mutharika
+Percentage=round(Totalvalidvotesfor_Bingumutharika*percent/Totalvalidvotes, 2);
+print("Total Valid Votes For Bingu Wa Mutharika in percentage=",Percentage)
+#Calculating percentage for John Tembo
+Percentage=round(Totalvalidvotesfor_Johntembo*percent/Totalvalidvotes, 2);
+print("Total Valid Votes For John Tembo in percentage=",Percentage)
+#Calculating percentage for Gwanda Chakuamba
+Percentage=round(Totalvalidvotesfor_Gwandachakuamba*percent/Totalvalidvotes, 2);
+print("Total Valid Votes For Gwanda Chakuamba in percentage=",Percentage)
+#Calculating percentage for Brown Mpinganjira 
+Percentage=round(Totalvalidvotesfor_Brownmpinganjira*percent/Totalvalidvotes, 2);
+print("Total Valid Votes For Brown Mpinganjira in percentage=",Percentage)
+#Calculating percentage for Jusice Malewezi
+Percentage=round(Totalvalidvotesfor_Justicemalewezi*percent/Totalvalidvotes, 2);
+print("Total Valid Votes For Jusice Malewezi in percentage=",Percentage)
+#
+print("============================================================================================")
